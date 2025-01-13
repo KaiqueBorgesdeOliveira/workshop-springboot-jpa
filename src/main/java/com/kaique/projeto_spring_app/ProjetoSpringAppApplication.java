dad0f3a05d0c547444996671dc9f37cbd55912b6
KaiqueBorgesdeOliveira <kaique.borges.1995@gmail.com>
2025-01-12 22:08:44 -0300

    Project created

---
+++ src/main/java/com/kaique/projeto_spring_app/ProjetoSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.kaique.projeto_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringAppApplication.class, args);
+	}
+
+}
